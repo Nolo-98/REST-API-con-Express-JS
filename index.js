1711300e3901fae95e0396266ae018525250fc60
Nolo-98 <twisterfast@gmail.com>
2023-06-03 13:49:00 -0600

    GET_Parametros query

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+ res.send('Hola mi server en express');
+});
 
 app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
 app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Producto 1',
-      price: 1000
-    },
-    {
-      name: 'Producto 2',
-      price: 2000
-    }
-  ]);
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+  products.push({
+    name : faker.commerce.productName(),
+    price : parseInt(faker.commerce.price(), 10),
+    image: faker.image.imageUrl(),
+  });
+}
+  res.json(products);
+});
+
+app.get('/users' , (req , res) => {
+  const { limit , offset } = req.query;
+  if (limit && offset) {
+    res.json({
+limit,
+offset
+    });
+  } else {
+res.send('No hay parametros');
+  }
 });
 
 app.get('/products/:id', (req, res) => {
