d7b9d6e0933c14c6a9d30f3594cc147925fea03a
Nolo-98 <twisterfast@gmail.com>
2023-06-05 01:48:51 -0600

    Middleware para HttpErrors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes');
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
@@ -17,26 +18,8 @@ app.get('/nueva-ruta', (req, res) => {
 
 routerApi(app);
 
-//app.get('/users' , (req , res) => {
- // const { limit , offset } = req.query;
-  //if (limit && offset) {
-    //res.json({
-//limit,
-//offset
-  //  });
-  //} else {
-//res.send('No hay parametros');
-  //}
-//});
-
-
-//app.get('/categories/:categoryId/products/:productId', (req, res) => {
- // const { categoryId, productId} = req.params;
-  //res.json({
-  //  categoryId,
-    //productId,
- // });
-//})
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('Mi port' + port);
