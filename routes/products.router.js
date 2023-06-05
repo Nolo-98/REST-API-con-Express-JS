da6939eb42de6cc9d6f22c8c40a6954cbe690f5b
Nolo-98 <twisterfast@gmail.com>
2023-06-05 00:02:50 -0600

    Codigos de estado o HTTP response status codes

--- routes/products.router.js
+++ routes/products.router.js
@@ -21,18 +21,24 @@ router.get('//filter' , (req, res ) => {
   res.send('Yo soy un filter');
 });
 
-router.get('//:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'Producto 2',
-    price: 2000
-  });
+  if (id  === '999') {
+    res.status(404).json({
+    message: 'not found'
+    });
+  } else {
+    res.status(200).json({
+      id,
+      name: 'Producto 2',
+      price: 2000
+    });
+  }
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
 message: 'created',
 data: body
   });
